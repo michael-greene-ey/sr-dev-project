be8e1a4de78dc3ccabc3b843a97989f8897e7602
michael-greene-ey <114023464+michael-greene-ey@users.noreply.github.com>
2023-07-28 14:21:27 -0500

    updates from pull request

--- src/assests/Constants.ts
+++ src/assests/Constants.ts
@@ -8,3 +8,4 @@ const BASE = "http://localhost:3000";
 
 export const HEALTH_CONTENT = `${BASE}/health-content`;
 export const HOMEPAGE = `${BASE}/homepage`;
+export const WORKOUT = `${BASE}/workouts`;
--- src/pages/HealthContent.tsx
+++ src/pages/HealthContent.tsx
@@ -31,23 +31,24 @@ const HealthContent = () => {
   return (
     <>
       <div className={styles.healthContent}>
-        <SingleCarousel
-          cardName={styles.title}
-          title="Health Articles"
-          data={pageCards || []}
-        ></SingleCarousel>
-        <SingleCarousel
-          cardName={styles.title}
-          title="Health Tips"
-          data={pageCards || []}
-        ></SingleCarousel>
-        <SingleCarousel
-          cardName={styles.title}
-          title="Health News"
-          data={pageCards || []}
-        ></SingleCarousel>
+        <div className={styles.overlay}>
+          <SingleCarousel
+            cardName={styles.title}
+            title="Health Articles"
+            data={pageCards || []}
+          ></SingleCarousel>
+          <SingleCarousel
+            cardName={styles.title}
+            title="Health Tips"
+            data={pageCards || []}
+          ></SingleCarousel>
+          <SingleCarousel
+            cardName={styles.title}
+            title="Health News"
+            data={pageCards || []}
+          ></SingleCarousel>
+        </div>
       </div>
-      <div className="image-overlay"></div>
     </>
   );
 };
